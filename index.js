060a62f959686ebe6547a0d7f5bcb2598c89000a
smatsu-5 <59785824+smatsu-5@users.noreply.github.com>
2021-11-17 16:00:37 -0500

    h

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 function flyout() {
     document.getElementsByClassName("test").style.display = "block";
 }
+
+function bigImg(x) {
+    x.style.height = "64px";
+    x.style.width = "64px";
+  }
+  
+  function normalImg(x) {
+    x.style.height = "32px";
+    x.style.width = "32px";
+  }
+  
