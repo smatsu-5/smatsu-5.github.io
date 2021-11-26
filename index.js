2197dda94e3c823f30a7757fd77761d870793a0d
smatsu-5 <59785824+smatsu-5@users.noreply.github.com>
2021-11-25 22:52:15 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,34 +1,11 @@
 const CLOSED_NAV_WIDTH = "3.7em";
 
-
-function flyout() {
-    document.getElementsByClassName("test").style.display = "block";
-}
-
-function bigImg() {
-    document.getElementById("sidebar").style.display = "block";
-    document.getElementById("sidebar").style.width = "22em";
-  }
-  
-  function normalImg() {
-    document.getElementById("sidebar").style.display = "none";
-  }
-
-
-  function openNav() {
+function openNav() {
     document.getElementById("side-nav").style.width = "10em";
     
     setTimeout(function(){ document.getElementById("list-group-icon").style.display = "none";
     document.getElementById("list-group-ul").style.display = "block"; }, 100);
     
-    
-    
-
-    /* Code to be executed if screen is mobile */
-    let isMobile = window.matchMedia("only screen and (max-width: 767px)").matches;
-    if(isMobile){
-    $('#mobile-apps-modal').modal('show');
-    }
 }
 
 /* Set the width of the side navigation to 0 */
